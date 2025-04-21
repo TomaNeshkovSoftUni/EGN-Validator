6180ba1cf176b91b1b60724722ac199b5668239c
TomaNeshkovSoftUni <toma.neshkov.highschool@buditel.bg>
2025-04-21 22:54:22 +0300

    demo
    unfinished

---
+++ ConsoleApp1/ConsoleApp1/EGNValidator.cs
@@ -0,0 +1,116 @@
+﻿using ConsoleApp13;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;  
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class EGNValidator : IEGNValidator
+    {
+        private int[] coefficients = { 2, 4, 8, 5, 10, 9, 7, 3, 6 };
+        public string[] Generate(DateTime birthDate, string city, bool isMale)
+        {
+            int year = birthDate.Year % 100;
+            int month = birthDate.Month;
+            int day = birthDate.Day;
+
+            if (birthDate.Year >= 1800 && birthDate.Year <= 1899)
+            {
+                month += 20;
+            }
+            else if (birthDate.Year >= 2000 && birthDate.Year <= 2099)
+            {
+                month += 40;
+            }
+
+            int genderNumber;
+            if (isMale)
+            {
+                genderNumber = new Random().Next(0, 500);
+            }
+            else
+            {
+                genderNumber = new Random().Next(500, 1000);
+            }
+
+            int randomDigits = new Random().Next(100, 1000);
+
+            string egnBase = $"{year:D2}{month:D2}{day:D2}{randomDigits:D3}";
+
+            int sum = 0;
+
+               for (int i = 0; i < 9; i++)
+            {
+                sum += int.Parse(egnBase[i].ToString()) * coefficients[i];
+            }
+
+            int controlDigit = sum % 11;
+            if (controlDigit == 10)
+                controlDigit = 0;
+
+            string finalEgn = egnBase + controlDigit.ToString();
+
+            return new string[] { finalEgn };
+        }
+            
+        public bool Validate(string egn)
+        {
+            if (egn.Length != 10 || !egn.All(char.IsDigit))
+            {
+                return false;
+            }
+
+            int year = int.Parse(egn.Substring(0, 2));
+            int month = int.Parse(egn.Substring(2, 2));
+            int day = int.Parse(egn.Substring(4, 2));
+
+            if (month >= 1 && month <= 12)
+            {
+                year += 1900;
+            }
+            else if (month >= 21 && month <= 32)
+            {
+                year += 1800;
+                month -= 20;
+            }
+            else if (month >= 41 && month <= 52)
+            {
+                year += 2000;
+                month -= 40;
+            }
+            else
+            {
+                return false;
+            }
+
+            try
+            {
+                DateTime time = new DateTime(year, month, day);
+            }
+            catch
+            {
+                return false;
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                sum += int.Parse(egn[i].ToString()) * coefficients[i];
+            }
+
+            int controlDigit = sum % 11;
+            if (controlDigit == 10)
+                controlDigit = 0;
+
+            int egnControlDigit = int.Parse(egn[9].ToString());
+
+            return controlDigit == egnControlDigit;
+
+            return true;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using ConsoleApp13;
+
+namespace ConsoleApp1
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            EGNValidator validator = new EGNValidator();
+
+            Console.WriteLine("Izberi opciq:");
+            Console.WriteLine("1. Validirane na EGN");
+            Console.WriteLine("2. Generirane na EGN");
+            Console.Write("Izbor: ");
+            string option = Console.ReadLine();
+
+            if (option == "1")
+            {
+                Console.Write("Vuvedi EGN: ");
+                string inputEgn = Console.ReadLine();
+
+                bool isValid = validator.Validate(inputEgn);
+                if (isValid)
+                    Console.WriteLine("EGN-to e validno!");
+                else
+                    Console.WriteLine("EGM-to NE e validno!");
+            }
+            else if (option == "2")
+            {
+                Console.Write("Vyvedi datata na rajdane (YYYY-MM-DD): ");
+                DateTime birthDate;
+                while (!DateTime.TryParse(Console.ReadLine(), out birthDate))
+                {
+                    Console.Write("Nevalidna data. Opitai otnovo: ");
+                }
+
+                Console.Write("Vuvedi grad: ");
+                string city = Console.ReadLine();
+
+                Console.Write("Vuvedi pol (m = male; f = female): ");
+                bool isMale = Console.ReadLine().ToLower() == "m";
+
+                try
+                {
+                    string[] egns = validator.Generate(birthDate, city, isMale);
+                    Console.WriteLine("Sgenerirano EGN:");
+                    foreach (var egn in egns)
+                    {
+                        Console.WriteLine(egn);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("GRESHKA: " + ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nevaliden izbor.");
+            }
+        }
+    }
+}
